bc8a96050036a6dec84a4c56f92ac938ea6dc84b
SezerGuvener <75906653+SezerGuvener@users.noreply.github.com>
2026-01-10 01:51:17 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+-- PLAKA OKUYUCUYU KULLANABİLEN ARAÇLAR
+-- Araç spawn name (model adı) girilir
+Config.AllowedVehicles = {
+    police2 = true,
+    -- police = true,
+    -- sheriff2 = true,
+}
